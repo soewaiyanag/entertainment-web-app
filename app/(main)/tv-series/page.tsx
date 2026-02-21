f913a034726f93e2f35fc0eac827dd900c1f2e2b
Soe Wai Yan Aung <soewaiyanag@gmail.com>
2026-02-21 17:35:31 +0700

    Implement TV series page with search

--- app/(main)/tv-series/page.tsx
+++ app/(main)/tv-series/page.tsx
@@ -1,7 +1,15 @@
+import data from "@/data.json";
+import { Show } from "@/types/show.type";
+import CategoryContent from "@/components/shows/category-content";
+
+const tvSeries = (data as Show[]).filter((s) => s.category === "TV Series");
+
 export default function TVSeriesPage() {
   return (
-    <div>
-      <h1>TVSeries Page</h1>
-    </div>
+    <CategoryContent
+      shows={tvSeries}
+      heading="TV Series"
+      searchPlaceholder="Search for TV series"
+    />
   );
 }
