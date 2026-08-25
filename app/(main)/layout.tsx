b0bece0587c4edf4b121b10ae4f06c71b989c8c0
Soe Wai Yan Aung <soewaiyanag@gmail.com>
2026-08-25 16:05:33 +0630

    Remove TMDB attribution footer

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -10,12 +10,6 @@ export default function MainLayout({ children }: PropsWithChildren) {
       <main className="px-4 pt-6 pb-16 md:px-6 lg:pl-[164px] lg:pr-8 lg:pt-16 lg:pb-16">
         {children}
       </main>
-      <footer className="px-4 pb-6 md:px-6 lg:pl-[164px] lg:pr-8 lg:pb-8">
-        <p className="text-preset-5 text-blue-500">
-          This product uses the TMDB API but is not endorsed or certified by
-          TMDB.
-        </p>
-      </footer>
     </div>
   );
 }
